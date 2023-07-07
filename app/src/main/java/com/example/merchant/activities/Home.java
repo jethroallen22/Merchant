1e0be2faf0c2af2cc3dbd42f046669fe4418f7ab
Technowaves000 <68849529+Technowaves000@users.noreply.github.com>
2023-07-07 14:23:01 +0800

    removed oncreateoptionsmenu

--- app/src/main/java/com/example/merchant/activities/Home.java
+++ app/src/main/java/com/example/merchant/activities/Home.java
@@ -340,12 +340,12 @@ public class Home extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.home, menu);
+//        return true;
+//    }
 
     @Override
     public boolean onSupportNavigateUp() {
